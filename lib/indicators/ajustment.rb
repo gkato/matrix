c72910679a44d4631add0541d4fed0cf58afbc93
Guilherme Kato <gkato@macbook-kato.home>
2017-06-20 20:09:41 -0300

    test fix

---
+++ lib/indicators/ajustment.rb
@@ -0,0 +1,37 @@
+require 'date'
+
+class Ajustment
+
+  attr_accessor :data
+
+  def initialize
+    @data = []
+  end
+
+  def add_data(historic_data)
+    start_date = DateTime.strptime("15/10/2016 15:50:00", "%d/%m/%Y %H:%M:%S")
+    end_date = DateTime.strptime("15/10/2016 16:00:00", "%d/%m/%Y %H:%M:%S")
+
+    if historic_data[:date].strftime("%H:%M:%S") < start_date.strftime("%H:%M:%S")
+      return
+    end
+    if historic_data[:date].strftime("%H:%M:%S") > end_date.strftime("%H:%M:%S")
+      return
+    end
+
+    @data << historic_data
+  end
+
+  def current_value
+    return if data.empty?
+
+    total_contracts = 0.0
+    total_value = 0.0
+    data.each do |historic|
+      total_contracts += historic[:qty].to_f
+      total_value += historic[:value].to_f * historic[:qty].to_f
+    end
+    return total_value / total_contracts
+
+  end
+end
---
+++ spec/specs/lib/indicators/adjustment_spec.rb
@@ -0,0 +1,50 @@
+require 'indicators/ajustment'
+
+describe Ajustment do
+ let(:historic) do
+   [
+     { date:DateTime.strptime("31/01/2017 09:00:01", "%d/%m/%Y %H:%M:%S"), value:3050, qty:1, ask:"A", bid:"B", :agressor=>"Comprador"},
+     { date:DateTime.strptime("31/01/2017 09:00:02", "%d/%m/%Y %H:%M:%S"), value:3051, qty:1, ask:"C", bid:"D", :agressor=>"Vendedor"},
+     { date:DateTime.strptime("31/01/2017 15:50:00", "%d/%m/%Y %H:%M:%S"), value:3049, qty:5, ask:"E", bid:"F", :agressor=>"Vendedor"},
+     { date:DateTime.strptime("31/01/2017 15:50:01", "%d/%m/%Y %H:%M:%S"), value:3050, qty:7, ask:"A", bid:"B", :agressor=>"Comprador"},
+     { date:DateTime.strptime("31/01/2017 15:54:02", "%d/%m/%Y %H:%M:%S"), value:3051, qty:8, ask:"C", bid:"D", :agressor=>"Vendedor"},
+     { date:DateTime.strptime("31/01/2017 15:58:03", "%d/%m/%Y %H:%M:%S"), value:3049, qty:3, ask:"E", bid:"F", :agressor=>"Vendedor"},
+     { date:DateTime.strptime("31/01/2017 16:00:00", "%d/%m/%Y %H:%M:%S"), value:3050, qty:4, ask:"A", bid:"B", :agressor=>"Comprador"},
+     { date:DateTime.strptime("31/01/2017 16:00:01", "%d/%m/%Y %H:%M:%S"), value:3051, qty:1, ask:"C", bid:"D", :agressor=>"Vendedor"},
+     { date:DateTime.strptime("31/01/2017 16:01:03", "%d/%m/%Y %H:%M:%S"), value:3049, qty:1, ask:"E", bid:"F", :agressor=>"Vendedor"}
+   ]
+ end
+ let(:subject) { Ajustment.new }
+
+ describe "#add_data" do
+   context "given a historic data with data lower than 15:50:00" do
+     it "ignores data" do
+       subject.add_data(historic.first)
+       expect(subject.data.size).to eq(0)
+     end
+   end
+   context "given a historic data with data greather than 16:00:00" do
+     it "ignores data" do
+       subject.add_data(historic.last)
+       expect(subject.data.size).to eq(0)
+     end
+   end
+   context "given a historic data with data between 15:50:00 and 16:00:00" do
+     it "adds data" do
+       subject.add_data(historic[3])
+       expect(subject.data.size).to eq(1)
+     end
+   end
+ end
+
+ describe "#current_value" do
+   describe "given a set of histoirc data containing data between 15:50:00 and 16:00:00" do
+     it "returns the current value" do
+       historic.each do |data|
+         subject.add_data(data)
+       end
+       expect(subject.current_value).to eq(3050)
+     end
+   end
+ end
+end
